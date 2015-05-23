974ff44acc010f4c64e6bf7f49a96c92a408147a
hiyu42 <hiyu0402@gmail.com>
2015-05-23 15:51:16 +0900

    UnityFirstCommit

---
+++ Assets/Arm.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arm : MonoBehaviour {
+	private GameObject MoobArm;
+	public const int SPEED = 5;
+
+	// Use this for initialization
+	void Start () {
+		//MoobArm = GameObject.Find ("MoobArm");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {   
+			transform.Translate (0, 0, 1);
+		}
+	}
+}
